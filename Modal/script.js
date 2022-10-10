a22bea13e9dece2f1c16fe8e62755cd8119c3339
Abu Jafar Md Jakaria <abujafar492@gmail.com>
2022-10-10 16:54:10 +0600

    add comments

--- Modal/script.js
+++ Modal/script.js
@@ -6,19 +6,25 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 // console.log(btnsOpenModal);
 
+//opening model
 const openModal = function () {
   console.log('Button Clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
+//closing model
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
+//loop for handling click events in all button
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
+//to close modal by clicking in closing button
 btnCloseModal.addEventListener('click', closeModal);
+
+//to close modal by clicking in overlay
 overlay.addEventListener('click', closeModal);
