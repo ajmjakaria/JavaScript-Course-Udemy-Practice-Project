efdd0bf903ea2d2224af59bc8744b307ce5b9b81
Abu Jafar Md Jakaria <abujafar492@gmail.com>
2022-10-10 13:11:28 +0600

    first commit

---
+++ Guess My Number/script.js
@@ -0,0 +1,18 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+document.querySelector('.number').textContent = 17;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 10;
+*/
+// document.querySelector('.check').addEventListener('click');
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+//Function for displaing message
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
