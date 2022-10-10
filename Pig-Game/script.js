14d1e3bd0e6fa51bbbe34416297b32672fe7afd8
Abu Jafar Md Jakaria <abujafar492@gmail.com>
2022-10-10 17:48:46 +0600

    select elements & set score 0

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
