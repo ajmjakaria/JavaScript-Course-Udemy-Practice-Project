e7e60c75c7c657173d6a0a48a64308262777151a
Abu Jafar Md Jakaria <abujafar492@gmail.com>
2022-10-11 15:36:31 +0600

    create roll dice function and generate random number

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -4,7 +4,22 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
+//Starting Conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //step 1: Generating Random Dice Roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  //step 2: Displaying Dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+});
