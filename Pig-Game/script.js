0965134cc9b1b95fcedb18e86565df541218e3e9
Abu Jafar Md Jakaria <abujafar492@gmail.com>
2022-10-11 16:44:36 +0600

    add dice rolling functionality

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -3,6 +3,9 @@
 //Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,6 +16,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   //step 1: Generating Random Dice Roll
@@ -22,4 +27,13 @@ btnRoll.addEventListener('click', function () {
   //step 2: Displaying Dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
+  //step3: Checking for rolled 1
+  if (dice !== 1) {
+    //Adding dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    //Switching to next player
+  }
 });
