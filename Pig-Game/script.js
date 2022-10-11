48d40c8e185c4de79e0e2c54fc734147d4f90244
Abu Jafar Md Jakaria <abujafar492@gmail.com>
2022-10-12 03:15:13 +0600

    add functionality of NEW GAME button and complete the pig game

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -13,15 +13,27 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-//Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  //Starting Conditions
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  diceEl.classList.add('hidden');
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -30,6 +42,8 @@ const switchPlayer = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+
+init();
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -55,6 +69,7 @@ btnRoll.addEventListener('click', function () {
   }
 });
 
+//Button Hold functionality
 btnHold.addEventListener('click', function () {
   if (playing) {
     //Step 1: Adding current score to active player's score
@@ -79,3 +94,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+//New game button functionality
+btnNew.addEventListener('click', init);
